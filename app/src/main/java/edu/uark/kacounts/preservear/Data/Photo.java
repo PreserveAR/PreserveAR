25f16e98deee3d3d9f9d950b252a9e3839904402
Kim <kacounts@uark.edu>
2021-12-05 15:26:36 -0600

    Added JSON file of our data
    Need to add GSON to enable unzipping upon opening app

--- app/src/main/java/edu/uark/kacounts/preservear/Data/Photo.java
+++ app/src/main/java/edu/uark/kacounts/preservear/Data/Photo.java
@@ -6,14 +6,18 @@ import androidx.room.PrimaryKey;
 
 @Entity
 public class Photo {
-    public static final String COMMENT = "comment";
     public static final String FNAME = "filename";
+    public static final String TITLE = "title";
+    public static final String COMMENT = "comment";
     public static final String ID = "id";
     public static final String LAT = "latitude";
     public static final String LONG = "longitude";
 
     @PrimaryKey(autoGenerate = true)
     private Integer id;
+
+    @ColumnInfo(name=TITLE)
+    private String title;
 
     @ColumnInfo(name=COMMENT)
     private String comment;
@@ -33,6 +37,14 @@ public class Photo {
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String comment) {
+        this.title = title;
     }
 
     public String getComment() {
