fe6c68f58f5c033d4c815d2f34a9043277e62033
hachristian <59273455+hachristian@users.noreply.github.com>
2021-12-06 08:29:23 -0600

    This one works

--- app/src/main/java/edu/uark/kacounts/preservear/PhotoActivity/MapsActivity.java
+++ app/src/main/java/edu/uark/kacounts/preservear/PhotoActivity/MapsActivity.java
@@ -14,7 +14,6 @@ import android.os.Bundle;
 import android.os.Looper;
 import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
